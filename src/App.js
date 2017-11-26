243e831cd796e95b6d4b1c3b6bef46e1b5a151dc
Andrew McMechan <coderwurst@users.noreply.github.com>
2017-11-26 12:15:22 +0100

    attempt to change logo colour

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Willkommen zum Brettspiel Banter Wahllokal</h1>
         </header>
         <p className="App-intro">
-          Willkommen zum Brettspiel Banter Wahllokal
+          Testing testing
         </p>
       </div>
     );
