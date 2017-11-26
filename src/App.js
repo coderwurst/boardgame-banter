b42e60c16273397a77c52dafb023db3ba3f6cda2
Andrew McMechan <coderwurst@users.noreply.github.com>
2017-11-26 21:35:15 +0100

    initial attempt at connect

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.sass';
 
+import sheetsConfig from '../config';
+
 // components
 import BoardgameTable from './components/BoardgameTable';
+import ReactGoogleSheetConnector from "react-google-sheet-connector"
 
 class App extends Component {
   render() {
@@ -13,7 +16,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Willkommen zum Brettspiel Banter Wahllokal</h1>
         </header>
-        <BoardgameTable></BoardgameTable>
+        
+        <ReactGoogleSheetConnector
+            clientid={sheetsConfig.clientid}
+            spreadsheetId={sheetsConfig.spreadsheetId}
+            spinner={ <div className="loading-spinner"/> } >
+            <div>
+              This content will be rendered once the data has been fetched from the spreadsheet.
+            </div>
+        </ReactGoogleSheetConnector>
+
+        
+      
       </div>
     );
   }
