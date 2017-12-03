404823a0f4bfb12567b16a5c70de33bfcf1c9728
Andrew McMechan <coderwurst@users.noreply.github.com>
2017-12-03 16:00:16 +0100

    update header

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Willkommen zum Brettspiel Banter Wahllokal</h1>
+          <h1 className="App-title">Willkommen zum Brettspiel Banter</h1>
         </header>
         <div class="table-wrapper">
           <ReactGoogleSheetConnector
